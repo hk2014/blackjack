a5006dc155aa7e95468e7ab62491cc02df5aeaea
shawnhr <shawnk723@live.com>
2017-10-18 10:46:20 -0700

    First EvenOccurence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -10,6 +10,43 @@
  * console.log(onlyEven); //  4
 */
 
+//1.spec:
+//input: Array of numbers
+//output: number
+//edge: return null if no even num
+
+//2.justification: to get the first-occured even number
+
+//3.explanation: function takes in array and gives the first even-occurrence
+
+//4.visual:
+
+//5.approximation: 
+//iterate array
+//check each elements to see if it's been occured previously
+// if it's been occured then check if their index is the same
+//if index is not the same then return the element of the index
+
+//6.verification: 
+//console.log(evenOccurrence([1,3,4,5,6]));
+
+
+//7.implementation
+
 var evenOccurrence = function(arr) {
   // Your code here.
+  for(var i = 0; i < arr.length; i++){
+  	for(var j = 0; j < arr.length; j++){
+  	if(arr[i] === arr[j]){
+  		if(arr.indexOf(arr[i]) !== arr.indexOf(arr[j])){
+  			 return arr[i];
+
+  		}
+  	}
+  }
+  	  //return null
+ }
+
+
 };
+console.log(evenOccurrence([1,3,4,5,6,6]));
--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,78 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+//spec:
+//in: number
+//out: array of strings
+
+
+//justification:
+//this function will be used  to list all the possible output
+
+//explantion:
+//in: function takes in number as an input in order for function to decide how many elements to give as output 
+
+
+//pseudo:
+//creat an empty arr
+//generate random value
+//populate each generated value as elements of the array
+//return the array
+
+
+var rockPaperScissors = function (val) {
   // TODO: your solution here
+  var cnt = val;
+  var combos = [];
+  var plays = ['rock', 'scissors', 'paper'];
+  var arr = [];
+
+  // var traverse = function(play){
+ 
+
+
+  
+  // 	while(plays.length){
+  // 		traverse(plays[plays.length])
+  //       plays.length--;
+  // 	}
+   
+for(var i = 0; i < cnt; i++){
+  for(var j = 0; j < plays.length; j++){
+    for(var h = 0; h < plays.length; h++){
+      for(var k = 0; k < plays.length; k++){
+        arr.push(plays[j],plays[h],plays[k])
+      }
+    }
+  }
+}
+console.log(arr);
 };
+
+
+console.log(rockPaperScissors(3))
+//   for(var i = 0; i < cnt; i++){
+//   	if(cnt > plays.length)
+//   		var leng = (cnt - plays.length-1);
+
+//   	arr.push(plays[leng])
+//   	else{
+//   		arr.push(plays[i])
+//   	}
+//   	for(var j = 0; j < cnt;j++)
+//   	{
+// 	    arr.push(plays[j]);
+	
+// 	  for(var h = 0; h < plays.length;h++){
+// 	  	arr.push(plays[h]);
+//     combos.push(arr)
+
+// 	  }
+
+//   }
+
+// }
+
+// console.log(combos);
+// };
+// rockPaperScissors(3);
