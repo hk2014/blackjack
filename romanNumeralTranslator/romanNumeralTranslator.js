55d01c5d315d627b79d3e1fbd4c1ac7596f9fea7
shawnhr <shawnk723@live.com>
2017-11-15 09:56:05 -0800

    roman

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -15,6 +15,7 @@
  *
  * You should return `null` on invalid input.
  */
+   
 
 var DIGIT_VALUES = {
   I: 1,
@@ -28,5 +29,25 @@ var DIGIT_VALUES = {
 
 var translateRomanNumeral = function(romanNumeral) {
 // TODO: Implement me!
+	 //if(!DIGIT_VALUES[])
 
+	  var num=0;  
+	  rom = romanNumeral.toUpperCase();
+	  var len = rom.length -1;
+
+	  for (var i = len; 0 <= i ; i--) {
+	    if (!DIGIT_VALUES[rom[i]]){
+	    	return "null";	
+	    }else{
+	    	if(DIGIT_VALUES[rom[i]] < DIGIT_VALUES[rom[i+1]]){
+	    		num -= DIGIT_VALUES[rom[i]];
+
+	    	}else{
+	    		num += DIGIT_VALUES[rom[i]];
+
+	    	}
+	    }
+	 }
+	  return num;  
 };
+console.log(translateRomanNumeral("lx") ); 
